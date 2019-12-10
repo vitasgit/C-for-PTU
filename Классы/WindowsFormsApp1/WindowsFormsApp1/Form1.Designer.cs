5a99f9d364739db757017da73c8ee666a39f60f3
vitasgit <vitasanon@mail.ru>
2019-12-10 11:22:50 +0500

    add Classes

---
+++ Классы/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class myForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hello = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hello
+            // 
+            this.hello.Location = new System.Drawing.Point(214, 185);
+            this.hello.Name = "hello";
+            this.hello.Size = new System.Drawing.Size(248, 77);
+            this.hello.TabIndex = 0;
+            this.hello.Text = "Click";
+            this.hello.UseVisualStyleBackColor = true;
+            this.hello.Click += new System.EventHandler(this.hello_Click);
+            // 
+            // myForm
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 392);
+            this.Controls.Add(this.hello);
+            this.Name = "myForm";
+            this.Text = "myForm";
+            this.Load += new System.EventHandler(this.myForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button hello;
+    }
+}
+
---
+++ Классы/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class myForm : Form
+    {
+        public myForm()
+        {
+            InitializeComponent();
+        }
+
+        private void myForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hello_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bonjour");
+        }
+    }
+}
